6f5749f37f3546553dd51d6669e93b329d7f35da
Fred Valle <phredvalle@gmail.com>
2019-09-06 16:29:12 -0500

    make labels inline using css

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>Countdown to December 25, 2019</div>
+        <div className="App-title">Countdown to December 25, 2019</div>
         <div>
-          <div>65 days</div>
-          <div>30 hours</div>
-          <div>15 minutes</div>
-          <div>20 seconds</div>
+          <div className="Clock-days">65 days</div>
+          <div className="Clock-hours">30 hours</div>
+          <div className="Clock-minutes">15 minutes</div>
+          <div className="Clock-seconds">20 seconds</div>
         </div>
         <input placeholder="new date" />
         <button>Submit</button>
