8cb2c3caacc8f6352431f1dd0cbcc14f7f8bf488
Fred Valle <phredvalle@gmail.com>
2019-09-06 16:23:48 -0500

    add basic styling using app.css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import './App.css';
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <div>Countdown to December 25, 2019</div>
-                <div>
-                    <div>65 days</div>
-                    <div>30 hours</div>
-                    <div>15 minutes</div>
-                    <div>20 seconds</div>
-                </div>
-            </div>            
-        )
-    }
+  render() {
+    return (
+      <div className="App">
+        <div>Countdown to December 25, 2019</div>
+        <div>
+          <div>65 days</div>
+          <div>30 hours</div>
+          <div>15 minutes</div>
+          <div>20 seconds</div>
+        </div>
+        <input placeholder="new date" />
+        <button>Submit</button>
+      </div>
+    );
+  }
 }
 
 export default App;
