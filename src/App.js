6ff17623227111d76d3b1863386f50c6e5204319
Fred Valle <phredvalle@gmail.com>
2019-09-06 16:19:23 -0500

    add hard coded divs for time placeholders

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import React, { Component } from 'react';
 class App extends Component {
     render() {
         return (
-            <div>Countdown Champ, App Component</div>
+            <div>
+                <div>Countdown to December 25, 2019</div>
+                <div>
+                    <div>65 days</div>
+                    <div>30 hours</div>
+                    <div>15 minutes</div>
+                    <div>20 seconds</div>
+                </div>
+            </div>            
         )
     }
 }
