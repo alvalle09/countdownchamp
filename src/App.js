7829fe46840cc9b18e1a8e5135057046c1c59963
alvalle09 <phredvalle@gmail.com>
2019-09-09 23:33:09 -0500

    set state

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React, { Component } from "react";
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+      super(props)
+      this.state = {
+          deadLine: 'December 26, 2017'
+      }
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-title">Countdown to December 25, 2019</div>
+        <div className="App-title">Countdown to {this.state.deadLine}</div>
         <div>
           <div className="Clock-days">65 days</div>
           <div className="Clock-hours">30 hours</div>
