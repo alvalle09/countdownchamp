6f8c40de408548179dc287810f9be04e43f4f225
alvalle09 <phredvalle@gmail.com>
2019-09-12 23:47:37 -0500

    infinite loop bug introduced, refactoring

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -17,6 +17,12 @@ class Clock extends Component {
     getTimeUntil(deadline) {
         const time =  Date.parse(deadline) - Date.parse(new Date());
         console.log('time', time);
+        const seconds = Math.floor((time/1000) % 60);
+        const minutes = Math.floor((time/1000/60) % 60);
+        const hours = Math.floor(time/(1000*60*60) % 24);
+        const days = Math.floor(time/(1000*60*60*24));
+        console.log('',seconds, ':seconds',minutes, ':minutes', hours, ':hours', days, 'days');
+        //this.setState({days: days});
     }
 
   render() {
