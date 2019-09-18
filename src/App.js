17182de30009db8d888dbf4dab52b651061d4d70
alvalle09 <phredvalle@gmail.com>
2019-09-17 22:14:10 -0500

    saving state for debuggin Form control aligns left

--- src/App.js
+++ src/App.js
@@ -1,42 +1,41 @@
 import React, { Component } from "react";
-import Clock from './Clock';
-import './App.css';
-import {Form, FormControl, Button} from 'react-bootstrap';
+import Clock from "./Clock";
+import "./App.css";
+import { Form, FormControl, Button } from "react-bootstrap";
 
 class App extends Component {
   constructor(props) {
-      super(props);
-      this.state = {
-          deadline: 'December 26, 2019',
-          newDeadline: ''
-          
-      }
+    super(props);
+    this.state = {
+      deadline: "December 26, 2019",
+      newDeadline: ""
+    };
   }
 
   changeDeadline() {
-      console.log('state', this.state);
-      this.setState({deadline: this.state.newDeadline});
+    console.log("state", this.state);
+    this.setState({ deadline: this.state.newDeadline });
   }
 
   render() {
     return (
       <div className="App">
         <div className="App-title">Countdown to {this.state.deadline}</div>
-        <Clock 
-          deadline={this.state.deadline}
-        />
+        <Clock deadline={this.state.deadline} />
+        {/* <Form inline> */}
         <Form inline>
+        <FormControl
+            className="Deadline-input"
+            placeholder="new date"
+            onChange={event =>
+              this.setState({ newDeadline: event.target.value })
+            }
+          />
+          <Button onClick={() => this.changeDeadline()}>Submit</Button>
+        {/* </Form> */}
 
-        
-        <FormControl 
-          classname="Deadline-input" 
-          placeholder="new date" 
-          onChange={event => this.setState({newDeadline: event.target.value})}
-        />
-        <Button onClick={() => this.changeDeadline()}>
-            Submit
-        </Button>
         </Form>
+          
       </div>
     );
   }
